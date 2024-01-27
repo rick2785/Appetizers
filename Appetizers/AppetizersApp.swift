9590870e885ee62ac43b273c962ca56f3070b400
rick2785 <rjhrabowskie@gmail.com>
2024-01-26 23:19:39 -0600

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by RJ Hrabowskie on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
